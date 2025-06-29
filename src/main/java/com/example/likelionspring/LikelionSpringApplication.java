2124fcd40623886df2e952d475d9e56682a184a7
yeilkk <yeil99@naver.com>
2025-06-30 01:25:33 +0900

    set: 프로젝트 세팅

---
+++ src/main/java/com/example/likelionspring/LikelionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.likelionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/likelionspring/LikelionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.likelionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
